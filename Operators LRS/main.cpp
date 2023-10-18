2091aaa28fe9cf2c22d48f150390647ba315a40c
tumbersa <gleb.kapustin.95@gmail.com>
2023-10-18 17:10:08 +0300

    Initial Commit

---
+++ Operators LRS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Operators LRS
+//
+//  Created by Глеб Капустин on 18.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
